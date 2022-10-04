1b394f5ba88c8c47887f8d07b2eb270df32c1932
Joel Maciel <soldado25461@gmail.com>
2022-10-04 18:51:26 -0300

    entity Offer implemented

--- backend/src/main/java/com/joel/learn/entities/Offer.java
+++ backend/src/main/java/com/joel/learn/entities/Offer.java
@@ -1,14 +1,14 @@
 package com.joel.learn.entities;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.Instant;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -19,21 +19,36 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "tb_course")
-public class Course implements Serializable{
+@Table(name = "tb_offer")
+public class Offer implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String name;
-	private String imgUri;
-	private String imgGrayUri;
+	private String edition;
+	private Instant startMoment;
+	private Instant endMoment;
 	
-	@OneToMany(mappedBy = "course")
-	private List<Offer> offers = new ArrayList<>();
+	@ManyToOne
+	@JoinColumn(name = "course_id")
+	private Course course;
+	
+	
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -15,6 +15,8 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Curso Java', 'https://chiptronic.com.br/blog/wp-content/uploads/2018/12/7-Cursos-para-chaveiros.jpg', 'https://chiptronic.com.br/blog/wp-content/uploads/2018/12/7-Cursos-para-chaveiros.jpg');
 
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2021-10-03T03:00:00Z', TIMESTAMP WITH TIME ZONE '2022-10-03T03:00:00Z', 1 );
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2021-12-03T03:00:00Z', TIMESTAMP WITH TIME ZONE '2022-09-03T03:00:00Z', 1 );
 
 
 
